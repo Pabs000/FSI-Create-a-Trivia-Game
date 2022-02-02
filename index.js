cd132ada69966e205b7128c00e2d9230bca9110b
Pabs000 <pgarcia2000.pg@gmail.comgit config --global user.name Pablo>
2022-02-02 15:11:11 -0800

    made 2 questions and answers for the minigame

--- index.js
+++ index.js
@@ -1,8 +1,59 @@
+
+
 // Your Code Here
+let userName = window.prompt('Pablo:')
+let userScore = 0
+let playAgain = true
 
-let userName = window.prompt('Pablo Garcia:')
-
+function playGame () {
 for(let i = 0; i < questions.length; i++){
     let question = questions[i]
-    window.prompt( _ )
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer === question.correctAnswer){
+        userScore = userScore + 10
+    }
+    {
+        text: `What is the first book of the Old Testament?
+        A. Leviticus
+        B. Numbers
+        C. Genesis
+        D. Exodus`,
+        correctAnswer: "C"
+        text: `What is the first letter of the alphabet?
+        A. A
+        B. B
+        C. C
+        D. D,
+        correctAnswer: "A"
+        text: `What is the first book of the Old Testament?
+        A. Leviticus
+        B. Numbers
+        C. Genesis
+        D. Exodus`,
+        correctAnswer: "C"
+        text: `What is the first book of the Old Testament?
+        A. Leviticus
+        B. Numbers
+        C. Genesis
+        D. Exodus`,
+        correctAnswer: "C"
+
+    }
+}
+
+window.alert('Your score is: '+userScore)
+
+}
+
+while (playAgain === true) {
+playGame()
+let userChoice = window.prompt('Would you like to play the game again? Answer yer or no.')
+if(userChoice --- 'yes'){
+    playAgain = true 
+}
+else{ 
+playAgain = false 
+window.alert('Thanks for playing the game!')
+}
+
 }
